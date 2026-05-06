9a352ee1971c9f1e650f446469607cfede893e3f
Edily Mora <edilyrosa@gmail.com>
2026-05-05 21:44:37 -0400

    clase 17 - fin modulo # 2

--- semana_2/flota_robots.py
+++ semana_2/flota_robots.py
@@ -16,9 +16,62 @@
 # │  una dependencia lógica — la forma correcta de modularizar.          │
 # └──────────────────────────────────────────────────────────────────────┘
 
-from robot import Robot     
+from robot import Robot, PI_2    
 class FlotaDeRobots:
-    pass
+    # aqui podria exi art de clase
+    #? Constructor
+    def __init__(self, nombre_empresa):
+        self.nombre_empresa = nombre_empresa
+        self.robots = [] #Lista de los robots de la flota
+    
+    #? metodos
+    def agregar_robot(self, nombre, tarea):
+        nuevo_robot = Robot(nombre, tarea)
+        self.robots.append(nuevo_robot)
+        print(f'➕Se agrego el robot {nombre} en la flota {self.nombre_empresa}')
+        
+    def saludar_todos_activos(self):
+        activos = [ r for r in self.robots if r.activo] #TODO: Listas comprehension
+        print(f'{self.nombre_empresa} - tiene {len( activos)} robots activos!!')
+        for robot in activos:
+            robot.saludar() #estamos llamado a un metodo de la clase Robot()
+        
+    def apagar_robot(self, nombre):
+        for r in self.robots:
+            if r.nombre == nombre:
+                r.apagar()
+                return
+        print(f'⚠️ No se encontro robots con el nombre {nombre}')
+    
+    def mostrar_estado(self):
+        print(f'\n\nESTADO DE LOS ROBOTS DE LA FLOTA { self.nombre_empresa}')
+        for robot in self.robots:
+            robot.estado() #estamos llamado a un metodo de la clase Robot()
+    
+    def resumen(self):
+        total = len(self.robots)
+        activos = sum(1 for r in self.robots if r.activo)
+        print(f'\n Resumen: total robots: {total} | {activos} Activos | {total - activos} Inactivos')
+        
+        
+        
+        
+armada_americana = FlotaDeRobots('Armada Americana!!')
+armada_rusa = FlotaDeRobots('Armada Rusa!!')
+armada_americana.agregar_robot('Robotina 1', 'canta para america')
+armada_americana.agregar_robot('Robotina 2', 'andar para america')
+armada_americana.agregar_robot('Robotina 3', 'limpiar para america')
+
+armada_americana.saludar_todos_activos()
+armada_americana.apagar_robot('Robotina 1')
+armada_americana.saludar_todos_activos()
+armada_americana.mostrar_estado()
+armada_americana.resumen()
+
+
+
+
+
 
 #& ─────────────────────────────────────────────────────────────────────
 #& CONCLUSIÓN SOBRE MODULARIDAD:
--- semana_2/robot.py
+++ semana_2/robot.py
@@ -25,12 +25,40 @@
 # Se define con la palabra clave "class" seguida de un nombre en formato PascalCase 
 # (primera letra de cada palabra en mayúscula). Por sí sola, la clase no hace nada; es solo el molde.
 
+PI_2 = 2.2
 class Robot:
    pass
    # TODO: Desarrola esto
-   # constructor(nombre, tarea) y ademas otro atr activo y sera bool, todos nacen activos
-   # saludar()
-   # apagar() → activos → false
-   # estado() → me informa si etsa activo o no
+   #? constructor(nombre, tarea) y ademas otro atr activo y sera bool, todos nacen activos
+   def __init__(self, nombre, tarea):
+      self.nombre = nombre  #Atr que paso por el constructor
+      self.tarea = tarea   #Atr que paso por el constructor
+      self.activo = True   #Atr defalult
    
-   # crea algunas instancias
+   #? saludar()
+   def saludar(self):
+      print(f'Hola soy {self.nombre} y mis tareas son: {self.tarea}')
+      
+   #? apagar() → activos → false
+   def apagar(self):
+      self.activo = False
+      print(f'❌{self.nombre} fue desactivado')
+      
+   #? estado() → me informa si esta activo o no
+   def estado(self):
+      estado_txt = '✅ Activo' if self.activo else '❌ Inactivo' #perador ternatio.
+      print(f'[{estado_txt}] - {self.nombre}')
+   
+   
+   
+#? crea algunas instancias
+mi_robot = Robot('edily', 'baila y cantar')
+mi_rotina = Robot('ronbotina', 'limpia oficinas')
+R2D2 = Robot('R2D2', 'Asiste a los Jadi')
+
+if __name__ == "__main__":
+   #las instancias llaman metodos 
+   mi_rotina.estado()
+   mi_rotina.apagar()
+   mi_rotina.estado()
+   mi_robot.saludar()
